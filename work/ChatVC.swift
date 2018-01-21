fc3e790ec81e111910fc5e1c5cfb1676b5902d23
Ranga Madushan <rangamadushan@Rangas-MacBook-Air.local>
2018-01-21 16:22:01 +0530

    send video and images successfully on to the chat

--- work/ChatVC.swift
+++ work/ChatVC.swift
@@ -14,19 +14,23 @@ import AVKit
 
 //AVKit for play video
 
-class ChatVC: JSQMessagesViewController {
+class ChatVC: JSQMessagesViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     private var messages = [JSQMessage]();
+    
+    let picker = UIImagePickerController();
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        picker.delegate = self;
+        
         self.senderId = "1";
         self.senderDisplayName = "ranga";
 
     }
     
-    //Begining of the collection View Functions
+    //START OF THE COLLECTION VIEW FUNCTION
     
     override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAt indexPath: IndexPath!) -> JSQMessageBubbleImageDataSource! {
         
@@ -45,6 +49,22 @@ class ChatVC: JSQMessagesViewController {
     }
     
     
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, didTapMessageBubbleAt indexPath: IndexPath!) {
+        
+        let msg = messages[indexPath.item];
+        
+        if msg.isMediaMessage {
+            if let mediaItem = msg.media as? JSQVideoMediaItem {
+                let player = AVPlayer(url: mediaItem.fileURL);
+                let playerController = AVPlayerViewController();
+                playerController.player = player;
+                self.present(playerController, animated: true, completion: nil);
+            }
+        }
+        
+    }
+    
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return messages.count;
     } //func of JSQ to load message similar like tableView
@@ -56,7 +76,10 @@ class ChatVC: JSQMessagesViewController {
         
     } //func of JSQ to load message similar like tableView
     
-    //end of the collection view functions
+    //END OF THE COLLECTION VIEW FUNCTION
+    
+    
+    // START OF THE SENDING BUTTON FUNCTIONS
     
     
     override func didPressSend(_ button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: Date!) {
@@ -68,6 +91,57 @@ class ChatVC: JSQMessagesViewController {
         //this will remove my typed text from the textfield
     }
     
+    override func didPressAccessoryButton(_ sender: UIButton!) {
+        
+        let alert = UIAlertController(title: "Media Messages", message: "Please Select A Media", preferredStyle: .actionSheet);
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil);
+        
+        let photos = UIAlertAction(title: "Photos", style: .default, handler: { (alert: UIAlertAction) in
+            self.chooseMedia(type: kUTTypeImage);
+        })
+        
+        let videos = UIAlertAction(title: "Videos", style: .default, handler: { (alert: UIAlertAction) in
+            self.chooseMedia(type: kUTTypeMovie);
+
+        })
+        
+        alert.addAction(photos);
+        alert.addAction(videos);
+        alert.addAction(cancel);
+        present(alert, animated: true, completion: nil);
+        
+    } //this one for sending images
+    
+    //END OF THE SENDING BUTTON FUNCTION
+    
+    
+    //START PICKER VIEW FUNCTION
+    
+    private func chooseMedia(type: CFString) {
+        picker.mediaTypes = [type as String]
+        present(picker, animated: true, completion: nil);
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let pic = info[UIImagePickerControllerOriginalImage] as? UIImage{
+        
+            let img = JSQPhotoMediaItem(image: pic);
+            self.messages.append(JSQMessage(senderId: senderId, displayName: senderDisplayName, media: img));
+            
+        }else if let vidUrl = info[UIImagePickerControllerMediaURL] as? URL{
+        
+            let video = JSQVideoMediaItem(fileURL: vidUrl, isReadyToPlay: true);
+            self.messages.append(JSQMessage(senderId: senderId, displayName: senderDisplayName, media: video));
+            
+        }
+        self.dismiss(animated: true, completion: nil);
+        collectionView.reloadData();
+    }
+    
+    //END PICKER VIEW FUNCTION
+
     
     @IBAction func backBtn(_ sender: AnyObject) {
         dismiss(animated: true, completion: nil);
